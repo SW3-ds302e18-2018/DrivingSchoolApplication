e2beb7b15a805c9c261396a0482e6835bd1fe251
Unknown <seraph14@hotmail.com>
2018-11-30 15:31:18 +0100

    Reverted order of enums in CourseType to master's

--- lesson-app/src/main/java/dk/aau/cs/ds302e18/app/domain/CourseType.java
+++ lesson-app/src/main/java/dk/aau/cs/ds302e18/app/domain/CourseType.java
@@ -1,8 +1,7 @@
 package dk.aau.cs.ds302e18.app.domain;
 
-public enum CourseType
-{
-    TYPE_A_BIKE,
+public enum  CourseType {
+    TYPE_B_CAR,
     TYPE_BE_CAR_TRAILER,
-    TYPE_B_CAR
+    TYPE_A_BIKE
 }
