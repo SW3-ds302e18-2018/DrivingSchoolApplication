38089c1f44e50ae23b8ed8f8ddbe18cd9460df06
Peter K <pkjar16@student.aau.dk>
2018-12-03 01:29:15 +0100

    Logbook done

---
+++ lesson-app/src/main/java/dk/aau/cs/ds302e18/app/domain/LogbookExportModel.java
@@ -0,0 +1,93 @@
+package dk.aau.cs.ds302e18.app.domain;
+
+public class LogbookExportModel
+{
+    private String username;
+    private String signatureUrl;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String signatureDate;
+    private String lessonId;
+    private String lessonType;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getSignatureUrl()
+    {
+        return signatureUrl;
+    }
+
+    public void setSignatureUrl(String signatureUrl)
+    {
+        this.signatureUrl = signatureUrl;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPosition()
+    {
+        return position;
+    }
+
+    public void setPosition(String position)
+    {
+        this.position = position;
+    }
+
+    public String getSignatureDate()
+    {
+        return signatureDate;
+    }
+
+    public void setSignatureDate(String signatureDate)
+    {
+        this.signatureDate = signatureDate;
+    }
+
+    public String getLessonId()
+    {
+        return lessonId;
+    }
+
+    public void setLessonId(String lessonId)
+    {
+        this.lessonId = lessonId;
+    }
+
+    public String getLessonType()
+    {
+        return lessonType;
+    }
+
+    public void setLessonType(String lessonType)
+    {
+        this.lessonType = lessonType;
+    }
+}
