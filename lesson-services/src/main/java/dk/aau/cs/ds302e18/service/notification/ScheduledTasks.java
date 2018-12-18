c915ffcfd61f79dd5b6a73815f803828995dde02
Peter K <pkjar16@student.aau.dk>
2018-12-18 15:20:10 +0100

    Fixed Scheduled tasks

--- lesson-services/src/main/java/dk/aau/cs/ds302e18/service/notification/ScheduledTasks.java
+++ lesson-services/src/main/java/dk/aau/cs/ds302e18/service/notification/ScheduledTasks.java
@@ -1,78 +1,74 @@
 package dk.aau.cs.ds302e18.service.notification;
 
-import dk.aau.cs.ds302e18.service.DBConnector;
+import dk.aau.cs.ds302e18.service.models.Account;
+import dk.aau.cs.ds302e18.service.models.AccountRespository;
+import dk.aau.cs.ds302e18.service.models.Lesson;
+import dk.aau.cs.ds302e18.service.models.LessonRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
-public class ScheduledTasks {
-    //private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+public class ScheduledTasks
+{
+    private AccountRespository accountRespository;
+    private LessonRepository lessonRepository;
+
+    public ScheduledTasks(AccountRespository accountRespository, LessonRepository lessonRepository)
+    {
+        this.accountRespository = accountRespository;
+        this.lessonRepository = lessonRepository;
+    }
 
     // This scheduler runs once every minute, therefore 60000 miliseconds
     @Scheduled(fixedRate = 60000)
-    public void scheduleSendLessonNotifications() {
+    public void scheduleSendLessonNotifications()
+    {
 
-        // Initialize MySQL connection
-        Connection conn = new DBConnector().createConnectionObject();
+        List<Account> accountList = this.accountRespository.findAll();
+        List<Lesson> lessonList = this.lessonRepository.findAll();
 
-        try {
-
-            Statement statement = conn.createStatement();
-
-            // Only select lessons that are still actual
-            ResultSet lessonResultSet = statement.executeQuery("SELECT `lesson_date`, `lesson_location`, `student_list` FROM `lesson` WHERE `lesson_date` >= NOW()");
-
-            while (lessonResultSet.next()) {
-
-                // Get the lesson date from the lesson, and set its seconds to 0 to correctly get a whole minute
-                Timestamp lessonDate = lessonResultSet.getTimestamp("lesson_date");
-                lessonDate.setSeconds(0);
-
-                // Retrieve the rest of the column values from the lesson table
-                String lessonLocation = lessonResultSet.getString("lesson_location");
-                String studentList = lessonResultSet.getString("student_list");
-                String[] studentListArray = studentList.split(",");
-
-                for(String studentUsername : studentListArray) {
-
-                    Statement usernameResultStatement = conn.createStatement();
-
-                    ResultSet usernameResultSet = usernameResultStatement.executeQuery("SELECT `email`, `phone_number`, `notification_reminder` FROM `account` WHERE `username` = '" + studentUsername + "'");
-
-                    while(usernameResultSet.next()) {
-
-                        // Retrieve the time in minutes before the student should receive a notification
-                        int notificationReminderInMinutes = usernameResultSet.getInt("notification_reminder");
-
-                        if (notificationReminderInMinutes == 0) // This student has set their notification reminder to 0, which means to skip them.
-                            continue;
-
-                        Timestamp currentTime = new Timestamp(System.currentTimeMillis()); // Create TimeStamp, set its nanoseconds and seconds to 0, along with setting it according to the notification time
-                        currentTime.setSeconds(0);
-                        currentTime.setNanos(0);
-                        currentTime = new Timestamp(currentTime.getTime() + notificationReminderInMinutes * 60 * 1000);
-
-                        if (!currentTime.equals(lessonDate))
-                            continue; // If the student is not supposed to receive a notification, go to the next student.
-
-                        // This student needs a notification. Retrieve the last two columns.
-                        String email = usernameResultSet.getString("email");
-                        String phoneNumber = usernameResultSet.getString("phone_number");
-
-                        String message = "Hello " + studentUsername + ",\n You have a lesson at " + lessonLocation + " on the\n" + lessonDate;
-                        new Notification(message, "ds302e18@gmail.com", email, "+45" + phoneNumber); // +45 is the region code for Denmark.
-                    }
-
-                    usernameResultSet.close();
+        for (Lesson lesson : lessonList)
+        {
+            for (Account account : accountList)
+            {
+                if (isApartofLesson(lesson, account) && withinNotificationTimer(lesson, account))
+                {
+                    System.out.println("TRIGGERED");
+                    String message = "Hello " + account.getUsername() + ",\n You have a lesson at " +
+                            lesson.getLessonLocation() + " on the\n" + lesson.getLessonDate();
+                    new Notification(message, "ds302e18@gmail.com", account.getEmail(),
+                            "+45" + account.getPhoneNumber());
+                    // +45 is the region code for Denmark.
                 }
-            }
 
-            lessonResultSet.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
         }
+
     }
+
+    private boolean isApartofLesson(Lesson lesson, Account account)
+        {
+            return lesson.getLessonInstructor().equals(account.getUsername()) ||
+                    lesson.getStudentList().contains(account.getUsername());
+        }
+
+        private boolean withinNotificationTimer(Lesson lesson, Account account)
+        {
+            Calendar today = Calendar.getInstance();
+
+            System.out.println("Current Time" + TimeUnit.MILLISECONDS.toMinutes(today.getTime().getTime()));
+            System.out.println("Lesson Time" + TimeUnit.MILLISECONDS.toMinutes(lesson.getLessonDate().getTime())+ "For Lesson " + lesson.getId());
+            System.out.println("Account Notification Minutes" + account.getNotificationInMinutes() + " For Account " + account.getUsername());
+
+            return TimeUnit.MILLISECONDS.toMinutes(lesson.getLessonDate().getTime()) - account.getNotificationInMinutes() ==
+                    TimeUnit.MILLISECONDS.toMinutes(today.getTime().getTime());
+
+        }
+
 }
+
